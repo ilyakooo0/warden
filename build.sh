592ede951764083aa986f514b221c8ad425dc3a1
iko <ilyakooo0@gmail.com>
2022-07-19 17:18:18 +0300

    wip

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-elm make ${DEBUG_BUILD:+--debug} "$SRC_DIR/src/Main.elm" --output="$INSTALL_DIR/index.html"
+elm make ${DEBUG_BUILD:+--debug} "$SRC_DIR/src/Main.elm" --output="$INSTALL_DIR/elm.js"
+
+rm "$SRC_DIR"/static/elm.js
 
 cp -r "$SRC_DIR"/static/* "$INSTALL_DIR"
