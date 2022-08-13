b191b74d4bf130847b199a008fbd2438659a17a5
iko <ilyakooo0@gmail.com>
2022-08-13 18:33:07 +0300

    Fixed

--- build.sh
+++ build.sh
@@ -10,4 +10,6 @@ elm make "$SRC_DIR/frontend/Main.elm" --optimize --output="$TEMP/elm.js"
 
 terser "$TEMP/elm.js" --ecma 2018 --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' | terser --ecma 2018 --mangle --compress --output "$INSTALL_DIR/elm.js"
 
-(cd "$SRC_DIR" && spago bundle-app --minify --to "$INSTALL_DIR/backend.js")
+(cd "$SRC_DIR" && spago bundle-app --to "$TEMP/backend.js")
+
+terser "$TEMP/backend.js" --ecma 2018 --mangle --compress --comments=false --toplevel --output "$INSTALL_DIR/backend.js"
